68748f03c0fa0f54ec4141baee3c0bc557df5f88
Aaron Long <aaron.long.c@gmail.com>
2017-08-04 11:27:18 -0400

    Began code first aspect of DB management

--- puzzle/src/Puzzle/Models/PuzzleModel.cs
+++ puzzle/src/Puzzle/Models/PuzzleModel.cs
@@ -1,9 +1,15 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Puzzle.Models
 {
+    [Table("puzzles")]
     public class PuzzleModel
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
         public string Problem { get; set; }
         public DateTime Added { get; set; }
     }
